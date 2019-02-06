aa54f134bdb775561b8448a1926fe7f941c76336
Simon Christ <SimonChrist@gmx.de>
2019-02-06 15:45:45 +0100

    update julia startup

--- dot_julia/config/startup.jl
+++ dot_julia/config/startup.jl
@@ -1,12 +1,13 @@
 using Logging
 isdebug[] = Ref(false)
+global_logger(ConsoleLogger(stderr,Logging.Info, show_limited = false))
 function DEBUG()
     if isdebug[]
-        global_logger(ConsoleLogger(stderr,Logging.Info))
+        global_logger(ConsoleLogger(stderr,Logging.Info, show_limited = false))
         isdebug() = false
         return nothing
     end
-    global_logger(ConsoleLogger(stderr,Logging.Debug))
+    global_logger(ConsoleLogger(stderr,Logging.Debug, show_limited = false))
     isdebug() = true
     return nothing
 end # function
