f7feefcbdecdf55b09465157b643eb433cc4af69
Simon Christ <SimonChrist@gmx.de>
2019-02-13 11:50:12 +0100

    bugfixes

--- dot_julia/config/startup.jl
+++ dot_julia/config/startup.jl
@@ -2,15 +2,16 @@ using Logging
 
 const PLOTS_DEFAULTS = Dict(:theme => :wong2)
 
-const isdebug = Ref(false)
+const ISDEBUG = Ref(false)
 global_logger(ConsoleLogger(stderr,Logging.Info, show_limited = false))
 function DEBUG()
-    if isdebug[]
+    global ISDEBUG
+    if ISDEBUG[]
         global_logger(ConsoleLogger(stderr,Logging.Info, show_limited = false))
-        isdebug() = false
+        ISDEBUG[] = false
         return nothing
     end
     global_logger(ConsoleLogger(stderr,Logging.Debug, show_limited = false))
-    isdebug() = true
+    ISDEBUG[] = true
     return nothing
 end # function
